5700b66592a5ce9298a0d6dd138d285b74cc63e5
AYUSH KUMAR SINGH <146700370+kali1706@users.noreply.github.com>
2025-09-14 18:00:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,6 @@ app = Flask(__name__, template_folder='.')
 def home():
     return render_template('index.html', data=CV_DATA)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
